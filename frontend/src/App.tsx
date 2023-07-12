bcb62ffa920f04ce9c12422e094578e9d8c4f015
Izak Voigt <izak.voigt@adgrowth.com>
2023-07-12 03:00:08 +0100

    Frontend-Feat: Update App.tsx and setup path alias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,19 @@
+import { TicketContextProvider } from '@contexts';
+import { Default } from '@pages';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { GlobalStyles } from './GlobalStyles';
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <TicketContextProvider>
+        <GlobalStyles />
+        <ToastContainer />
+        <Default />
+      </TicketContextProvider>
+    </>
+  );
 }
 
 export default App;
---
+++ frontend/src/compounds/index.ts
@@ -0,0 +1,7 @@
+export * from './CreateRandomTicketButton/index';
+export * from './CreateTicketButton/index';
+export * from './CreateTicketForm/index';
+export * from './DisplayTickets/index';
+export * from './ModalCreateTicket/index';
+export * from './Ticket/index';
+export * from './TicketStatus/index';
